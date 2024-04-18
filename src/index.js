a0d46f50f43b18b13359ff961c269b3850329ce6
leniero <leandronieroml@gmail.com>
2024-04-18 22:05:00 +0100

    functions-second-third-user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-//* Production code file
+//Production code file
 
-// export const addToBasket = (item) => {
+import basket from './basket.js';
+import item from './item.js';
+
+//user story 1
+export const addToBasket = (item) => {
+    basket.push(item)
+    return true;
+};
+
+//user story 2
+export const removeFromBasket = (item) => {
+    basket.pop(item)
+    return true;
+}
+
+//user story 3
+export const isFull = () => {
+    if (basket.length === 10) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+//user story 4
+// export const increaseCapacity = (basket)=> {
+//     basket.length += 1;
 //     return true;
-// };
-
+// }
